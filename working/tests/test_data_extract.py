773f7be086c4d850a98a4476b7eefc3bb099ae85
Sara A <saralnjd@uw.edu>
2021-06-04 00:30:56 -0700

    pep8

--- working/tests/test_data_extract.py
+++ working/tests/test_data_extract.py
@@ -1,9 +1,9 @@
-
 import sqlite3
 import pandas as pd
 import unittest
 
 import sys
+
 sys.path.append('../')
 import data_extract
 
@@ -11,7 +11,8 @@ import data_extract
 class Testdataextract(unittest.TestCase):
     def test_dbfile_to_dataframe(self):
         result = list(data_extract.dbfile_to_dataframe('source.db').columns)
-        self.assertEqual(result, ['organism', 'standard_type', 'standard_value', 'standard_units', 'chembl_id', 'canonical_smiles']), 'unexpected result'
+        self.assertEqual(result, ['organism', 'standard_type', 'standard_value', 'standard_units', 'chembl_id',
+                                  'canonical_smiles']), 'unexpected result'
         return
 
     def test_merged_data(self):
@@ -19,6 +20,6 @@ class Testdataextract(unittest.TestCase):
         data2 = pd.read_csv("Ovis_Aries.csv", sep=';')
         df = data_extract.merged_data(data1, data2)
         result = list(df['organism'].unique())
-        self.assertEqual(result,['Oryctolagus cuniculus', 'Ovis aries']), 'unexpected result: the dataframes are not combined properly'
+        self.assertEqual(result, ['Oryctolagus cuniculus',
+                                  'Ovis aries']), 'unexpected result: the dataframes are not combined properly'
         return
-
