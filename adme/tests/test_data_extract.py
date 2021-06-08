a2ec18fb5382f2a5d7bca3ae3fd9ab71934ef200
Liwen Xing <liwenx@uw.edu>
2021-06-08 16:10:08 -0700

    debug tests

--- adme/tests/test_data_extract.py
+++ adme/tests/test_data_extract.py
@@ -9,7 +9,7 @@ from adme import data_extract
 
 class Testdataextract(unittest.TestCase):
     def test_dbfile_to_dataframe(self):
-        result = list(data_extract.dbfile_to_dataframe('source.db').columns)
+        result = list(data_extract.dbfile_to_dataframe('tests/source.db').columns)
         self.assertEqual(result, [
             'organism', 'standard_type', 'standard_value',
             'standard_units', 'chembl_id',
@@ -18,8 +18,8 @@ class Testdataextract(unittest.TestCase):
         return
 
     def test_merged_data(self):
-        data1 = pd.read_csv("Oryctolagus_cuniculus.csv", sep=';')
-        data2 = pd.read_csv("Ovis_Aries.csv", sep=';')
+        data1 = pd.read_csv("tests/Oryctolagus_cuniculus.csv", sep=';')
+        data2 = pd.read_csv("tests/Ovis_Aries.csv", sep=';')
         df = data_extract.merged_data(data1, data2)
         result = list(df['organism'].unique())
         self.assertEqual(result, ['Oryctolagus cuniculus',
