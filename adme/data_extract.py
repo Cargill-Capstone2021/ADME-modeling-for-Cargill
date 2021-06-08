85f679d02cbe137896f2d7c17d82eec193d5d99f
Sara A <saralnjd@uw.edu>
2021-06-07 17:38:37 -0700

    pep8

--- adme/data_extract.py
+++ adme/data_extract.py
@@ -13,22 +13,21 @@ import pandas as pd
 
 def database_file_to_data_frame(db):
     """
-    Creates a SQL connection to the SQLite. This function reads SQL database files,
-    executes SQL queries in python platform and extracts data for specific table and
-    returns a pandas DataFrame
-    
+    Creates a SQL connection to the SQLite.This function reads
+    SQL database files, executes SQL queries in python
+    and extracts data for specific table and returns a DataFrame
     Parameters
     ----------
     - db: database file/ full path of database file
 
-    
     Returns
     -------
     - modified DataFrame based on the user selected tables and columns
     """
 
     con = sqlite3.connect(db)
-    # The desired sql query could be written in """ """ to join the desired table and columns
+    # The desired sql query could be written in """ """
+    # to join the desired table and columns
     df = pd.read_sql_query("""SELECT * FROM source""", con)
 
     return df
@@ -38,12 +37,11 @@ def database_file_to_data_frame(db):
 
 
 def merged_data(df1, df2):
-    """This function combines two DataFrames
-    
+    """
+    This function combines two DataFrames
     Parameters
     ----------
     - df1,df2: DataFrames to be combined
-    
     Returns
     -------
     - merged DataFrame
