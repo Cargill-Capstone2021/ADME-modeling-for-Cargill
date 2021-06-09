706adb649f1447726cb2018087ec429fd1577afd
Liwen Xing <liwenx@uw.edu>
2021-06-09 15:25:30 -0700

    debug

--- adme/tests/test_Descriptors.py
+++ adme/tests/test_Descriptors.py
@@ -1,34 +1,28 @@
 from adme.modules import Descriptors
 import numpy as np
 import pandas as pd
-import unittest
 
-def test_generate(self):
-  """
-  Unit test for generating molecular descriptors.
-  """
-  df = pd.read('df_Cmax_test.csv')
-  input = df.canonical_smiles
-  columnNames=["MolWt","NumRotatableBonds"] 
-  baseData= np.arange(1,1)
-  output = Descriptors.generate(input)
-  result = pd.DataFrame(data=baseData,columns=columnNames)
-  expected = pd.DataFrame
-  self.assertTrue(result, excpected)
 
-  return
-   
+def test_generate():
+    """
+    Unit test for generating molecular descriptors.
+    """
+    df = pd.read_csv('tests/df_Cmax_test.csv', sep=';')
+    input = df['canonical_smiles']
+    columnNames = ["MolWt", "NumRotatableBonds"]
+    baseData = np.arange(1, 1)
+    output = Descriptors.generate(input)
+    result = pd.DataFrame(data=baseData, columns=columnNames)
+    expected = pd.DataFrame
+    assert result.equals(expected), "The function is broken!"
 
-def test_generate_1(self):
 
-  df = pd.read('df_Cmax_test.csv')
-  input = df.canonical_smiles
-  columnNames=["MolMR","HeavyAtomCount","HeavyAtomMolwt"]   
-  baseData= np.arange(1,1)
-  output = Descriptors.generate(input)
-  result = pd.DataFrame(data=baseData,columns=columnNames)
-  expected = pd.DataFrame
-  self.assertTrue(result, excpected)
-
-  return
- 
+def test_generate_1():
+    df = pd.read_csv('tests/df_Cmax_test.csv', sep=';')
+    input = df['canonical_smiles']
+    columnNames = ["MolMR", "HeavyAtomCount", "HeavyAtomMolwt"]
+    baseData = np.arange(1, 1)
+    output = Descriptors.generate(input)
+    result = pd.DataFrame(data=baseData, columns=columnNames)
+    expected = pd.DataFrame
+    assert result.equals(expected), "The function is broken!"
